867c2c33da14d1ca70fa8d3868f242e09cc83cbf
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-01-02 11:58:48 +0530

    added Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/About'
+import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -9,7 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
-
+      <Features/>
     </main>
   )
 }
